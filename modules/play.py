2ddf8b693c9200df16096c2bb2eec9fd5cd2f357
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-08-18 23:58:31 +0530

    Update play.py

--- modules/play.py
+++ modules/play.py
@@ -843,8 +843,9 @@ async def ytplay(_, message: Message):
         await message.reply_photo(
             photo="final.png",
             reply_markup=keyboard,
-            caption=f"▶️ **song by** {r_by.mention}\n\n**🏷️Title** {title} \n\n**⏳Duration** {duration}\n\n**👁️Views** {views}\n\n",
-        ),
+            caption="▶️ **song by {r_by.mention}\n**🏷️Title{title}**\n**⏳Duration {duration}**\n**👁️Views{views}**\n".format(
+                message.from_user.mention()
+            ),
         )
         os.remove("final.png")
         return await lel.delete()
@@ -1227,7 +1228,7 @@ async def lol_cb(b, cb):
             photo="final.png",
             reply_markup=keyboard,
             caption=f"▶️ **song by** {r_by.mention}\n\n**🏷️Title** {title} \n\n**⏳Duration** {duration}\n\n**👁️Views** {views}\n\n",
-        )
+          )
         
         os.remove("final.png")
 
